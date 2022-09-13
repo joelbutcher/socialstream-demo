67b005f6184a4cd92c648a696b9c69e12fceaa19
Joel Butcher <joel@clicktech.com>
2022-09-13 13:15:08 +0100

    fix missing data

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,27 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Models\Team;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
-
 Route::middleware([
     'auth:sanctum',
 ])->name('api.')->group(function () {
-    Route::get('/user', fn (Request $request) => $request->user());
+    Route::get('/user', fn () => request()->user());
+
+    Route::get('/users', fn () => response()->json(
+        User::query()
+            ->whereNot('id', request()->user()->id())
+            ->paginate()
+            ->appends(request()->query())
+    ))->name('users.index');
+
+    Route::get('/teams', fn () => response()->json(
+        Team::query()
+            ->select(['teams.*'])
+            ->leftJoin('team_user', 'teams.id', '=', 'team_user.team_id')
+            ->where('teams.user_id', request()->user()->id)
+            ->orWhere('team_user.user_id', request()->user()->id)
+            ->paginate()
+            ->appends(request()->query())
+    ))->name('teams.index');
 });
