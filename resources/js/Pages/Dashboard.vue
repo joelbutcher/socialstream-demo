cc510b0e81497b1a13959a2354ef9b52130fef01
Joel Butcher <joel@joelbutcher.co.uk>
2022-09-28 16:46:33 +0100

    Fix dashboard title

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -7,8 +7,8 @@ import Welcome from '@/Components/Welcome.vue';
 <template>
   <AppLayout title="Dashboard">
     <template #header>
-      <div class="max-w-7xl mx-auto pt-10 md:px-4 lg:px-8">
-        <h2 class="font-bold text-xl md:text-2xl text-slate-800 dark:text-slate-200 leading-tight">
+      <div class="max-w-7xl mx-auto pt-10 px-4 lg:px-8">
+        <h2 class="font-bold text-xl md:text-2xl text-slate-800 dark:text-slate-200 leading-tight mt-2">
           Dashboard
         </h2>
       </div>
