277b8e00dd0cecaf940585f6ee82f1b31ae071d6
Joel Butcher <joel.butcher@gosuperscript.com>
2023-11-07 10:10:39 +0000

    fix: Apple OAuth

--- app/Actions/Socialstream/ResolveSocialiteUser.php
+++ app/Actions/Socialstream/ResolveSocialiteUser.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Socialstream;
 
+use App\Services\Apple\AppleToken;
 use JoelButcher\Socialstream\Contracts\ResolvesSocialiteUsers;
 use JoelButcher\Socialstream\Socialstream;
 use Laravel\Socialite\Contracts\User;
@@ -9,16 +10,26 @@ use Laravel\Socialite\Facades\Socialite;
 
 class ResolveSocialiteUser implements ResolvesSocialiteUsers
 {
+    public function __construct(
+        private readonly AppleToken $appleToken
+    ) {
+        //
+    }
+
     /**
      * Resolve the user for a given provider.
      */
     public function resolve(string $provider): User
     {
+        config()->set('services.apple.client_secret', $this->appleToken->generate());
+
         $user = Socialite::driver($provider)->user();
 
         if (Socialstream::generatesMissingEmails()) {
             $user->email = $user->getEmail() ?? ("{$user->id}@{$provider}".config('app.domain'));
         }
+
+        config()->set('services.apple.client_secret', null);
 
         return $user;
     }
