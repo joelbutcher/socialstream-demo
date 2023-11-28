da344bf6c12cc0caba7f19b9de865603509a30c4
Joel Butcher <joel.butcher@gosuperscript.com>
2023-11-28 12:50:52 +0000

    fix: table sequences out of sync bug

--- app/Console/Commands/Refresh.php
+++ app/Console/Commands/Refresh.php
@@ -9,6 +9,7 @@ use App\Notifications\Slack\SlackAppRefreshFailedNotification;
 use App\Notifications\Slack\SlackAppRefreshSuccessfulNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Throwable;
 
@@ -30,6 +31,11 @@ class Refresh extends Command
 
         // Cleanup
         Storage::deleteDirectory('database/exports');
+
+        // Fixes pgsql sequences out of sync issues.
+        DB::raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users)+1)");
+        DB::raw("SELECT setval('teams_id_seq', (SELECT MAX(id) FROM teams)+1)");
+        DB::raw("SELECT setval('connected_accounts_id_seq', (SELECT MAX(id) FROM connected_accounts)+1)");
 
         SlackAppRefreshSuccessfulNotification::send();
 
