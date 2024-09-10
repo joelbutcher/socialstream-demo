432ac38c2343108c2d69ee415724a3cd9e7cc8c4
Joel Butcher <joelbutcher@users.noreply.github.com>
2024-09-10 13:48:19 +0100

    first

--- config/services.php
+++ config/services.php
@@ -35,4 +35,15 @@ return [
         ],
     ],
 
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => '/login/oauth/github/callback',
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => '/login/oauth/google/callback',
+    ],
 ];
